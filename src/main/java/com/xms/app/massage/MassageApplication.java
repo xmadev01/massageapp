40bc5755ad7deb938f0b904c0bf1c44181307b3c
unknown <jeremy.pu@gmail.com>
2021-03-19 10:52:11 +1100

    commit all files

---
+++ src/main/java/com/xms/app/massage/MassageApplication.java
@@ -0,0 +1,13 @@
+package com.xms.app.massage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MassageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xms/app/massage/MassageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xms.app.massage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
